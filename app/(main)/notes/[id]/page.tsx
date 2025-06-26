08eadec27baa2fc8ff5af4c02934e6c557ef3ba6
Albert Wu <albertwuinston@gmail.com>
2025-06-26 11:20:54 +0700

    revert the changes

--- app/(main)/notes/[id]/page.tsx
+++ app/(main)/notes/[id]/page.tsx
@@ -1,16 +1,18 @@
-// app/(main)/notes/[id]/page.tsx
+'use client'
+
 export const dynamicParams = true;
+import { use } from 'react';
+import NotesDetail from '@/components/match-notes/NotesDetail';
 
-import ClientWrapper from './ClientWrapper';
-
-export default function Note({ params }: { params: { id: string } }) {
-  const id = params.id;
+export default function Note(props: { params: Promise<{ id: string }> }) {  
+  const { id } = use(props.params);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] min-h-screen gap-2 p-8 sm:p-8 xl:p-20 xl:pt-8 font-[family-name:var(--font-geist-sans)]">
-      <div className="mt-4">
-        <ClientWrapper id={id} />
-      </div>
-    </div>
+        <div className="grid grid-rows-[20px_1fr_20px] min-h-screen gap-2 p-8 sm:p-8 xl:p-20 xl:pt-8 font-[family-name:var(--font-geist-sans)]">
+            <div className='mt-4'>
+                {/* <MatchForm/> */}
+                { id && <NotesDetail id={id}/>}
+            </div>
+        </div>
   );
 }
