d8c0fb1dbd744b2b1d8bdf068ba6b1f322f93a20
Albert Wu <albertwuinston@gmail.com>
2025-06-26 10:21:18 +0700

    fix SSR

--- app/(main)/notes/[id]/page.tsx
+++ app/(main)/notes/[id]/page.tsx
@@ -1,9 +1,8 @@
 export const dynamicParams = true;
-import { use } from 'react';
 import NotesDetail from '@/components/match-notes/NotesDetail';
 
-export default function Note(props: { params: Promise<{ id: string }> }) {  
-  const { id } = use(props.params);
+export default function Note({ params }: { params: { id: string } }) {
+  const id = params.id;
 
   return (
         <div className="grid grid-rows-[20px_1fr_20px] min-h-screen gap-2 p-8 sm:p-8 xl:p-20 xl:pt-8 font-[family-name:var(--font-geist-sans)]">
